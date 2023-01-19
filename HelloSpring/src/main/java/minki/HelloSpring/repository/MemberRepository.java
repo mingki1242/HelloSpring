113380006bce4c14fc2bcd0a7ac146fb4647bd89
qkral <qkralsrl214@naver.com>
2023-01-20 00:12:05 +0900

    first spring

---
+++ HelloSpring/src/main/java/minki/HelloSpring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package minki.HelloSpring.repository;
+import minki.HelloSpring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+}
