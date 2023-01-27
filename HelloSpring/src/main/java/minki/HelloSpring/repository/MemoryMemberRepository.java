216f896c22e6366e80892d84ff594530d4c2f2b0
qkral <qkralsrl214@naver.com>
2023-01-27 16:15:58 +0900

    Test

--- HelloSpring/src/main/java/minki/HelloSpring/repository/MemoryMemberRepository.java
+++ HelloSpring/src/main/java/minki/HelloSpring/repository/MemoryMemberRepository.java
@@ -22,9 +22,10 @@ public class MemoryMemberRepository implements MemberRepository{
 
     @Override
     public Optional<Member> findByName(String name) {
-        return store.values().stream()
-                .filter(member -> member.getName().equals(name))
-                .findAny();
+        return store.values().stream() //stream 객체 생성
+                .filter(member -> member.getName().equals(name)) //filter로 가공
+                //member 객체에서 이름을 가져온뒤 name과 이름이 같은 것을 탐색
+                .findAny();//찾으면 반환
     }
 
     @Override
---
+++ HelloSpring/src/test/java/minki/HelloSpring/repository/MemoryMemberRepositoyTest.java
@@ -0,0 +1,13 @@
+package minki.HelloSpring.repository;
+
+import org.junit.jupiter.api.Test;
+
+public class MemoryMemberRepositoyTest {
+    MemberRepository mr = new MemoryMemberRepository();
+
+    @Test
+    public void save()
+    {
+
+    }
+}
